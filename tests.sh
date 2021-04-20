10354ba46a10c17ea5c2ec90967db46a52c28342
badconfig <array.clean@gmail.com>
2021-04-20 17:10:14 +0300

    sdf

--- tests.sh
+++ tests.sh
@@ -1,3 +1,10 @@
 curl -v -d '{"login":"usr1","auth_type":"plain","roles":["admin","seller"]}' --header "Content-Type: application/json" -X PUT "http://localhost:8088/internal/auth"
 
 curl -v -d '{"login":"usr1","auth_type":"plain"}' --header "Content-Type: application/json" -X GET "http://localhost:8088/internal/auth"
+
+
+# reglog
+curl -v -d '{"mail":"array.clean@gmail.com","password":"pass"}' --header "Content-Type: application/json" -X GET "http://localhost:8089/public/reglogbackend/login"
+
+
+curl -v -d '{"mail":"array.clean@gmail.com","password":"pass","verifyed":true}' --header "Content-Type: application/json" -X PUT "http://localhost:8089/public/reglogbackend/register"
