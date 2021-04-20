fe1259ba6e6222a14738b3df37a4065a631fa83c
Your Name <you@example.com>
2021-04-20 17:05:57 +0300

    dsaf

--- deployment/init-letsencrypt.sh
+++ deployment/init-letsencrypt.sh
@@ -8,8 +8,8 @@ fi
 domains=(redrufus.art www.redrufus.art)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="array.clean@gmail.com" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
